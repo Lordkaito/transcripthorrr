775d32a3949b866c2fedeaaa20fc0b10095690a2
Lordkaito <cespedesisai88@gmail.com>
2023-10-05 12:26:13 +0200

    Initial commit with basic conf

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from modules import module1, module2
+import audio_transcriber
+import librosa
+import audio_transcriber_pro
+from werkzeug.exceptions import RequestEntityTooLarge
+
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+CORS(app, origins=["http://localhost:5173"])
+# CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/')
+
+def index():
+    return jsonify({'message': 'Hello, World!'})
+
+@app.route("/transcribe", methods=["POST"])
+def transcribe():
+  audio_file = request.files.get("audio_file")
+  audio_data, sr = librosa.load(audio_file)
+  result = audio_transcriber.transcribe_audio(audio_data)
+  # print(type(audio_data))
+  print(result)
+  return jsonify({'message': result})
+
+@app.errorhandler(RequestEntityTooLarge)
+def handle_request_entity_too_large(error):
+    print(error)
+    return jsonify({'error': 'El archivo es demasiado grande. El tamaño máximo permitido es de 16 MB.'}), 400
+
+
+@app.route("/transcribe_pro", methods=["POST"])
+def transcribe_pro():
+  print(request)
+  audio_file = request.files.get("audio_file")
+  audio_data, sr = librosa.load(audio_file)
+
+  audio_thing = audio_file.read()
+  print(len(audio_thing))
+  result = audio_transcriber_pro.transcribe_audio(audio_data)
+  print(result)
+  return jsonify({'message': result})
+
+
+def allowed_file(file):
+  return "." in file and file.rsplit(".", 1)[1].lower() in {"wav", "mp3"}
+
+
+if __name__ == '__main__':
+    app.run()
